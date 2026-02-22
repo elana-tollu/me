b4cc4c9a3836185ff0b1f9ed0b2395bceed47e48
Elana Tollu <elana.tollu@yandex.ru>
2026-02-22 20:59:06 +0100

    pr-1: add progress lambda

---
+++ lambdas/progress/src/handler.ts
@@ -0,0 +1,42 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+    DynamoDBDocumentClient,
+    QueryCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = "test-weight";
+
+export const handler = async () => {
+
+    const query = new QueryCommand({
+        TableName: tableName,
+        KeyConditionExpression: "PK = :pk",
+        ExpressionAttributeValues: {
+            ":pk": "mysh_weight"
+        }
+    });
+
+    const dbResponse = await dynamo.send(query);
+
+    if (!dbResponse.Items) {
+        throw new Error(`Failed to send dynamo response from DynamoDB ${dbResponse}`);
+    }
+
+    const dayWeights = dbResponse.Items.map((item) => ({
+        date: item.SK,
+        weight: item.weight
+    }));
+
+    /*
+     * Generate HTTP response using 200 status code with a simple body.
+     */
+    const response = {
+        dayWeights
+    };
+
+    return response;
+};
