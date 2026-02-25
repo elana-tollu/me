c21a82934b8527c5185f9fb497117df6269de565
Elana Tollu <elana.tollu@yandex.ru>
2026-02-25 22:59:32 +0100

    pr-1: add lambda

--- infra/amplify/backend.ts
+++ infra/amplify/backend.ts
@@ -1,9 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
+import {progressLambda} from "./functions/progress/resourse";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
 defineBackend({
   auth,
+  progressLambda,
 });
