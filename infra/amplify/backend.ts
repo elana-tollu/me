867fa8d9ac82c39b344b0a7fa76961dc9fcd801b
Elana Tollu <elana.tollu@yandex.ru>
2026-02-27 21:35:34 +0100

    pr-1: create empty backend

--- infra/amplify/backend.ts
+++ infra/amplify/backend.ts
@@ -1,11 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import {progressLambda} from "./functions/progress/resourse";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
-  auth,
-  progressLambda,
-});
+const backend = defineBackend({});
